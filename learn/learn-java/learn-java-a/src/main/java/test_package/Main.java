e653f773debffb8ac099e4d64a365487498b9c95
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-04-08 01:02:05 +0700

    up-to tutorial 45

--- learn/learn-java/learn-java-a/src/main/java/test_package/Main.java
+++ learn/learn-java/learn-java-a/src/main/java/test_package/Main.java
@@ -1,5 +1,7 @@
 package test_package;
 
+import java.util.Scanner;
+
 public class Main 
 {
     public static void main( String[] args )
@@ -10,10 +12,18 @@ public class Main
         String testString = "GGWP";
         testString += "HH";
         System.out.println( testString.toLowerCase() );
-
         System.out.println(testByte + testShort + testLong);
-        
+
         Func01.sayName();
         Func01.sayAge();
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("What's your favorite number? : ");
+        System.out.println("Oh, It's " + sc.nextDouble() + ". Cool!");
+
+        sc.close();
+
+
     }
 }
