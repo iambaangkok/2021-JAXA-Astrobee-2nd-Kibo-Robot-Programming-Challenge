06d07a560be03b991d6742ddf0963edc60d4b7e9
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-04-07 11:12:37 +0700

    up-to tutorial 36

--- learn/learn-java/learn-java-a/src/main/java/test_package/Func01.java
+++ learn/learn-java/learn-java-a/src/main/java/test_package/Func01.java
@@ -4,4 +4,11 @@ public class Func01 {
     public static int mod17(int num) {
         return num%17;
     }
+
+    public static void sayName(){
+        System.out.println("BK");
+    }
+    public static void sayAge(){
+        System.out.println(19);
+    }
 }
--- learn/learn-java/learn-java-a/src/main/java/test_package/Main.java
+++ learn/learn-java/learn-java-a/src/main/java/test_package/Main.java
@@ -4,7 +4,16 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( Func01.mod17(19) );
+        byte testByte = 127;
+        short testShort = 32767;
+        long testLong = 9000000000000L;
+        String testString = "GGWP";
+        testString += "HH";
+        System.out.println( testString.toLowerCase() );
+
+        System.out.println(testByte + testShort + testLong);
+        
+        Func01.sayName();
+        Func01.sayAge();
     }
 }
