8c19572b73098dbcec4542ee99d71b752d301729
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-04-28 19:06:36 +0700

    Added a fresh SampleApk to work on

---
+++ infinitus-loopus/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -0,0 +1,65 @@
+package jp.jaxa.iss.kibo.rpc.sampleapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+        // astrobee is undocked and the mission starts
+        api.startMission();
+
+        // astrobee is undocked and the mission starts
+        moveToWrapper(11.71, -9.53, 5.35, 0, 0, 0, 1);
+
+        // irradiate the laser
+        api.laserControl(true);
+
+        // take snapshots
+        api.takeSnapshot();
+
+        // move to the rear of Bay7
+        moveToWrapper(10.275, -10.314, 4.295, 0, -0.7071068, 0, 0.7071068);
+
+        // Send mission completion
+        api.reportMissionCompletion();
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write here your plan 2
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write here your plan 3
+    }
+
+    // You can add your method
+    private void moveToWrapper(double pos_x, double pos_y, double pos_z,
+                               double qua_x, double qua_y, double qua_z,
+                               double qua_w){
+
+        final int LOOP_MAX = 3;
+        final Point point = new Point(pos_x, pos_y, pos_z);
+        final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
+                                                     (float)qua_z, (float)qua_w);
+
+        Result result = api.moveTo(point, quaternion, true);
+
+        int loopCounter = 0;
+        while(!result.hasSucceeded() || loopCounter < LOOP_MAX){
+            result = api.moveTo(point, quaternion, true);
+            ++loopCounter;
+        }
+    }
+
+}
+
