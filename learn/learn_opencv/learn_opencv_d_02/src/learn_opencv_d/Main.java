cfd304ce1cfae8481452f6f27b0da89573b0cebf
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-04-30 20:32:26 +0700

    OpenCV Console test grounds

--- learn/learn_opencv/learn_opencv_d_02/src/learn_opencv_d/Main.java
+++ learn/learn_opencv/learn_opencv_d_02/src/learn_opencv_d/Main.java
@@ -1,7 +1,19 @@
 package learn_opencv_d;
-	
-import org.opencv.core.Core;
 
+import org.opencv.core.Core;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Rect;
+import org.opencv.calib3d.Calib3d;
+// opencv library
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+// java library
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
@@ -17,8 +29,72 @@ public class Main extends Application {
 	public static void main(String[] args){
 		// load the native OpenCV library
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	
 		
-		launch(args);
+		System.out.println("[image]: "+ "loading");
+		Mat image = new Mat(); 
+		image = Imgcodecs.imread("D:\\GitHub\\JAXA-2nd-Kibo-RPC\\learn\\learn_opencv\\learn_opencv_d\\src\\apigetMatNavCam.png", Imgcodecs.IMREAD_GRAYSCALE);
+		//System.out.println("[image]: "+ image.dump());
+		Rect roi = new Rect(1,1,1,1);
+		Mat undistortedPic = undistort(image,roi);
+		Imgcodecs.imwrite("D:\\GitHub\\JAXA-2nd-Kibo-RPC\\learn\\learn_opencv\\learn_opencv_d\\src\\undistortedImage.png", undistortedPic);
+		System.out.println("[roi]: "+ roi.toString());
+		
+		Mat croppedPic = new Mat();
+        cropImage(undistortedPic, croppedPic, roi);
+        Imgcodecs.imwrite("D:\\GitHub\\JAXA-2nd-Kibo-RPC\\learn\\learn_opencv\\learn_opencv_d\\src\\croppedImage.png", croppedPic);
+        
+        
+		//launch(args);
+	}
+	
+	private static void cropImage(Mat sourceImage, Mat targetMat, Rect roi){
+        System.out.println("cropImage[roi]: "+ String.valueOf(roi));
+        Mat croppedImage = new Mat(sourceImage,roi);
+        croppedImage.copyTo(targetMat);
+        return;
+    }
+	
+	
+	public static Mat undistort(Mat sourceImage, Rect roi) {
+		System.out.println("undistortImage[status]: "+ "start");
+        
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		double[][] navCamIntrinsics  = {{567.229305, 0.0, 659.077221, 0.0, 574.192915, 517.007571, 0.0, 0.0, 1.0}, {-0.216247, 0.03875, -0.010157, 0.001969, 0.0}};
+		
+		
+        System.out.println("undistortImage[status]: "+ "got navCamIntrinsics " + Arrays.deepToString(navCamIntrinsics));
+        int row = 0, col = 0;
+        Mat cameraMat = new Mat(3, 3, CvType.CV_32FC1);
+        cameraMat.put(row, col, navCamIntrinsics[0]);
+        System.out.println("undistortImage[status]: "+ "cameraMat put " + cameraMat.toString() + " " + cameraMat.dump());
+
+        row = 0; col = 0;
+        Mat distortionCoeff = new Mat(1, 5, CvType.CV_32FC1);
+        distortionCoeff.put(row,col,navCamIntrinsics[1]);
+        System.out.println("undistortImage[status]: "+ "distortCoeff put " + distortionCoeff.toString() + " " + distortionCoeff.dump());
+
+        int test = 1;
+        Mat undistortedPic = new Mat(1280, 960, CvType.CV_8UC1);
+
+        System.out.println("undistortImage[sourceImage.size()]: " +  sourceImage.size().toString());
+        System.out.println("undistortImage[cameraMat.size()]: " +  cameraMat.size().toString());
+        System.out.println("undistortImage[distortionCoeff.size()]: " +  distortionCoeff.size().toString());
+        Size sourceImageSize = sourceImage.size();
+        if(test == 1){
+            Mat newCameraMat = Calib3d.getOptimalNewCameraMatrix(cameraMat, distortionCoeff, sourceImageSize, 1, sourceImageSize, roi);
+            System.out.println("undistortImage[status]: "+ "got optimalNewCamMat");
+            System.out.println("undistortImage[status]: "+ "got optimalNewCamMat " + newCameraMat.dump());
+            System.out.println("undistortImage[newCameraMat.size()]: " +  newCameraMat.size().toString());
+
+            Calib3d.undistort(sourceImage, undistortedPic, cameraMat, distortionCoeff, newCameraMat);// (sourceImage,undistortedPic,cameraMat,distortionCoeff,newCameraMat);
+            System.out.println("undistortImage[status]: "+ "finished fisheye_undistort");
+        }else if(test == 2){ //This method crashes the program
+            //Imgproc.undistort(sourceImage, undistortedPic, cameraMat, distortionCoeff);
+            System.out.println("undistortImage[status]: "+ "finished Imgproc.undistort");
+        }
+        
+        return undistortedPic;
 	}
 	
 	
