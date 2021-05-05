c4bc02c936fc09a408d18412d6b2bde62dc85bd6
DarkTXYZ <pawaretmontfort@gmail.com>
2021-05-05 12:12:54 +0700

    Update YourService.java

--- infinitus-loopus/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ infinitus-loopus/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,4 +1,4 @@
-package jp.jaxa.iss.kibo.rpc.thailand;
+package jp.jaxa.iss.kibo.rpc.sampleapk;
 
 import gov.nasa.arc.astrobee.Kinematics;
 import gov.nasa.arc.astrobee.Result;
@@ -33,13 +33,13 @@ import java.util.List;
 
 public class YourService extends KiboRpcService
 {
-    String MODE = "iss"; // mode setting ("sim" or "iss")
+    String MODE = "sim"; // mode setting ("sim" or "iss")
     int NAV_MAX_COL = 1280;
     int NAV_MAX_ROW =  960;
     int PointCloud_COL = 224;
     int PointCloud_ROW = 171;
     // carmera constant value
-    int max_count = 5, center_range = 6, P1 = 0, P2 = 1;
+    int max_count = 3, center_range = 6, P1 = 0, P2 = 1;
     // limit value
     float AR_diagonal = 0.07071067812f;
     float ARtoTarget = 0.1414f, y_shift = 0.1328f;
@@ -121,6 +121,7 @@ public class YourService extends KiboRpcService
 
         moveTo((float)x_org - (float)sx, (float)y_org, (float)z_org + (float)sy, (float)a, (float)b, (float)c, (float)w);
     }
+
     public Mat undistord(Mat src)
     {
         Mat dst = new Mat(1280, 960, CvType.CV_8UC1);
@@ -129,46 +130,22 @@ public class YourService extends KiboRpcService
 
         int row = 0, col = 0;
 
-        double cameraMatrix_sim[] =
-                {
-                        344.173397, 0.000000, 630.793795,
-                        0.000000, 344.277922, 487.033834,
-                        0.000000, 0.000000, 1.000000
-                };
-        double distCoeffs_sim[] = {-0.152963, 0.017530, -0.001107, -0.000210, 0.000000};
+        double cameraMatrix_sim[] = api.getNavCamIntrinsics()[0];
+        double distCoeffs_sim[] = api.getNavCamIntrinsics()[1];
 
-        double cameraMatrix_orbit[] =
-                {
-                        692.827528, 0.000000, 571.399891,
-                        0.000000, 691.919547, 504.956891,
-                        0.000000, 0.000000, 1.000000
-                };
-        double distCoeffs_orbit[] = {-0.312191, 0.073843, -0.000918, 0.001890, 0.000000};
-
-        if(MODE == "sim")
-        {
-            cameraMatrix.put(row, col, cameraMatrix_sim);
-            distCoeffs.put(row, col, distCoeffs_sim);
-            Log.d("Mode[camera]:"," sim");
-        }
-        else if(MODE == "iss")
-        {
-            cameraMatrix.put(row, col, cameraMatrix_orbit);
-            distCoeffs.put(row, col, distCoeffs_orbit);
-            Log.d("Mode[camera]:"," iss");
-        }
-
-        cameraMatrix.put(row, col, cameraMatrix_orbit);
-        distCoeffs.put(row, col, distCoeffs_orbit);
+        cameraMatrix.put(row, col, cameraMatrix_sim);
+        distCoeffs.put(row, col, distCoeffs_sim);
+        Log.d("Mode[camera]:"," sim");
 
         Imgproc.undistort(src, dst, cameraMatrix, distCoeffs);
         return dst;
     }
+
     public Rect cropImage(int percent_crop)
     {
-        double ratio = NAV_MAX_COL / NAV_MAX_ROW;
+        double ratio = (double)NAV_MAX_COL / NAV_MAX_ROW;
 
-        double percent_row = percent_crop/2;
+        double percent_row = percent_crop/2.0;
         double percent_col = percent_row * ratio;
 
         int offset_row = (int) percent_row * NAV_MAX_ROW / 100;
@@ -178,6 +155,7 @@ public class YourService extends KiboRpcService
 
         return new Rect(offset_col, offset_row, (int) cols, (int) rows);
     }
+
     public Bitmap resizeImage(Mat src, int width, int height)
     {
         Size size = new Size(width, height);
@@ -187,65 +165,12 @@ public class YourService extends KiboRpcService
         matToBitmap(src, bitmap, false);
         return bitmap;
     }
-    public double[] Intersection(double p[][])
-    {
-        double[] center = new double[3];
 
-        double a = (p[1][0] - p[0][0]) * (p[3][0] - p[2][0]);
-        double b = (p[1][0] - p[0][0]) * (p[3][1] - p[2][1]);
-        double c = (p[3][0] - p[2][0]) * (p[1][1] - p[0][1]);
-
-        center[0] = (a * p[0][1] + b * p[2][0] - a * p[2][1] - c * p[0][0]) / (b - c);
-        center[1] = ((p[1][1] - p[0][1]) * (center[0] - p[0][0]) / (p[1][0] - p[0][0])) + p[0][1];
-
-        double x_l1 = Math.pow(p[0][0] - p[1][0], 2);
-        double y_l1 = Math.pow(p[0][1] - p[1][1], 2);
-        double x_l2 = Math.pow(p[3][0] - p[2][0], 2);
-        double y_l2 = Math.pow(p[3][1] - p[2][1], 2);
-        double avg = (Math.sqrt(x_l1 + y_l1) + Math.sqrt(x_l2 + y_l2)) / 2;
-
-        center[2] = avg / AR_diagonal;
-        Log.d("AR[info]: ", center[0] + ", " + center[1] + ", " + center[2]);
-        return center;
-    }
-    public double getPointCloud(int center_range)
-    {
-        double depth = 0;
-        int count = 0;
-
-        Log.d("PointCloud[status]:", " start");
-        PointCloud hazCam = api.getPointCloudHazCam();
-        Point[] point = hazCam.getPointArray();
-        int width  = hazCam.getWidth();
-        int height = hazCam.getHeight();
-        int row_max = height/2 + center_range/2;
-        int row_min = height/2 - center_range/2;
-        int col_max = width/2  + center_range/2;
-        int col_min = width/2  - center_range/2;
-        Log.d("PointCloud[status]:", " stop");
-
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        for (int row = row_min; row < row_max; row++)
-        {
-            for (int col = col_min; col < col_max; col++)
-            {
-                depth += point[(row * width) + col].getZ();
-                count++;
-            }
-        }
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        depth /= count;
-        Log.d("PointCloud[value]:", "z[" + depth + "]");
-        return depth;
-    }
     public void flash_control(boolean status)
     {
         if(status)
         {
-            api.flashlightControlFront(0.025f);
+            api.flashlightControlFront(0.3f);
 
             try
             {
@@ -258,6 +183,7 @@ public class YourService extends KiboRpcService
         }
         else api.flashlightControlFront(0);
     }
+
     public double[] QR_event(float px, float py, float pz, float qx, float qy, float qz, float qw, int count_max, int no)
     {
         String contents = null;
@@ -268,15 +194,13 @@ public class YourService extends KiboRpcService
         {
             Log.d("QR[status]:", " start");
             long start_time = SystemClock.elapsedRealtime();
-            //                                            //
+
             moveTo(px, py, pz, qx, qy, qz, qw);
             Log.d("QR[NO]:"," "+no);
 
             flash_control(true);
             Mat src_mat = new Mat(undistord(api.getMatNavCam()), cropImage(40));
             Bitmap bMap = resizeImage(src_mat, 2000, 1500);
-
-
 
             //////////////////////////////////////////////////////////////////////////////////////////////////////
             int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
@@ -305,14 +229,11 @@ public class YourService extends KiboRpcService
             Log.d("QR[status]:", " stop");
             long stop_time = SystemClock.elapsedRealtime();
 
-
-
             Log.d("QR[count]:", " " + count);
             Log.d("QR[total_time]:"," "+ (stop_time-start_time)/1000);
             count++;
         }
-
-
+        
         flash_control(false);
         api.sendDiscoveredQR(contents);
         return new double[] {final_x, final_y, final_z, final_w};
